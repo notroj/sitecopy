2b2d042635c0710f148213826f53f68bc2e7b990
Joe Orton <notroj@users.noreply.github.com>
2005-01-03 10:33:28 +0000

    * .release.sh: Really run .update-po.sh.

--- .release.sh
+++ .release.sh
@@ -5,10 +5,11 @@
 echo $1 > .version
 
 # Generate docs for Xsitecopy
-cd gnome/doc
+pushd gnome/doc
 db2html -o `pwd` xsitecopy.sgml
 # Remove generated images dir
 rm xsitecopy/stylesheet-images/*.gif
 rmdir -p xsitecopy/stylesheet-images
+popd
 
 exec ./.update-po.sh
