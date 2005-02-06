3f39eb39fa3734700f20603849580b348a3640fc
joe <joe@61a7d7f5-40b7-0310-9c16-bb0ea8cb1845>
2005-02-06 20:57:33 +0000

    * .update-po.sh: Do clean up temp files.
    * .release.sh: Generate sitecopy.spec.

--- sitecopy/trunk/.release.sh
+++ sitecopy/trunk/.release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -ex
+#!/bin/bash -ex
 # Release script run before generating a release tarball.
 # Usage: ./.release.sh VERSION
 
@@ -11,5 +11,7 @@ db2html -o `pwd` xsitecopy.sgml
 rm xsitecopy/stylesheet-images/*.gif
 rmdir -p xsitecopy/stylesheet-images
 popd
+
+sed s/@VERSION@/$1/ < sitecopy.spec.in > sitecopy.spec
 
 exec ./.update-po.sh
