69a6f366c74bbc7d80d434fb8009507fa47e7b7d
joe <joe@61a7d7f5-40b7-0310-9c16-bb0ea8cb1845>
2008-07-16 15:59:53 +0000

    * .update-po.sh: Fix for current gettext.

--- sitecopy/trunk/.update-po.sh
+++ sitecopy/trunk/.update-po.sh
@@ -3,14 +3,14 @@
 # a release tarball.  Run from .release.sh, *before*
 # autogen.sh. 
 
-inmk=/usr/share/gettext/po/Makefile.in.in
+inmk=Makefile.in.in
 tmpmk=`mktemp /tmp/sitecopy.XXXXXX`
 pot=`mktemp /tmp/sitecopy.XXXXXX`
 trap 'rm -f $tmpmk $pot' TERM INT 0
 
 cd po
 
-lngs=`echo *.po | sed "s/.po//g"`
+lngs=`cat LINGUAS`
 for l in $lngs; do
     CATALOGS="$CATALOGS $l.gmo"
     POFILES="$POFILES $l.po"
@@ -38,6 +38,7 @@ s/@GMSGFMT@/msgfmt/g;
 s/@MSGFMT@/msgfmt/g;
 s/@XGETTEXT@/xgettext/g;
 s/@MSGMERGE@/msgmerge/g;
+s/@XGETTEXT_EXTRA_OPTIONS@/--msgid-bugs-address sitecopy@webdav.org/g;
 s/: Makefile.*/:/g;
 s/\$(MAKE) update-gmo/echo Done/g;" $inmk > $tmpmk
 
