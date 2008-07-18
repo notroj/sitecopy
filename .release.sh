2674614453774204971dfc01f0a2a24ca25a267c
Joe Orton <notroj@users.noreply.github.com>
2008-07-18 08:46:58 +0000

    * Makefile.in, .release.sh: More gnomefe removal.

--- .release.sh
+++ .release.sh
@@ -4,14 +4,6 @@
 
 echo $1 > .version
 
-# Generate docs for Xsitecopy
-pushd gnome/doc
-db2html -o `pwd` xsitecopy.sgml
-# Remove generated images dir
-rm xsitecopy/stylesheet-images/*.gif
-rmdir -p xsitecopy/stylesheet-images
-popd
-
 sed s/@VERSION@/$1/ < sitecopy.spec.in > sitecopy.spec
 
 exec ./.update-po.sh
