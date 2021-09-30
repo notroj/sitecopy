40777a7a5881224f060bd85aaeafefa45b8b104b
Joe Orton <joe@manyfish.uk>
2021-09-30 21:44:54 +0100

    First pass at getting working buildsystem again. - only external neon/libintl supported - bundle config.guess/sub for now

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,9 @@
 #!/bin/sh -ex
 rm -rf config.cache autom4te*.cache
 test -f .version || echo -n 0.0.0 > .version
+mkdir -p m4/neon
+curl -s 'https://raw.githubusercontent.com/notroj/neon/master/macros/neon.m4' > m4/neon/neon.m4
+curl -s 'https://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/ax_type_socklen_t.m4' > m4/neon/socklen.m4
 ${ACLOCAL:-aclocal} -I m4/neon
 ${AUTOHEADER:-autoheader}
 ${AUTOCONF:-autoconf}
-cp /usr/share/libtool/config.sub .
-cp /usr/share/libtool/config.guess .
-rm -rf autom4te*.cache
-rm -rf intl
-cp -pr /usr/share/gettext/intl intl
-
