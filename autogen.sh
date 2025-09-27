2da0f875d690f0fb706b18426cfd1aa05937eddb
Joe Orton <joe@manyfish.uk>
2025-09-27 08:07:59 +0100

    Import neon as a submodule.

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,9 @@
-#!/bin/sh -ex
-rm -rf config.cache autom4te*.cache
-test -f .version || echo -n 0.0.0 > .version
-mkdir -p m4/neon
-curl -s 'https://raw.githubusercontent.com/notroj/neon/master/macros/neon.m4' > m4/neon/neon.m4
-curl -s 'https://git.savannah.gnu.org/gitweb/?p=autoconf-archive.git;a=blob_plain;f=m4/ax_type_socklen_t.m4' > m4/neon/socklen.m4
-${ACLOCAL:-aclocal} -I m4/neon
+#!/bin/sh
+set -ex
+rm -rf config.cache autom4te*.cache aclocal.m4
+${ACLOCAL:-aclocal} -I m4 -I neon/macros
 ${AUTOHEADER:-autoheader}
 ${AUTOCONF:-autoconf}
+${LIBTOOLIZE:-libtoolize} --copy --force >/dev/null
+rm -rf autom4te*.cache
+
