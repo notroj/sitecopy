9ee210b3a98b955ec37729e9aadf9852bb9b843e
Joe Orton <joe@manyfish.uk>
2025-12-23 11:09:50 +0000

    * autogen.sh: Fix build with Fedora gettext-devel.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 set -ex
 rm -rf config.cache autom4te*.cache aclocal.m4
-${ACLOCAL:-aclocal} -I m4 -I neon/macros
+INCLUDES="-I m4 -I neon/macros"
+
+for d in /usr/share/gettext/m4; do
+    if test -d "$d"; then
+        INCLUDES="$INCLUDES -I ${d}"
+    fi
+done
+
+${ACLOCAL:-aclocal} ${INCLUDES}
 ${AUTOHEADER:-autoheader}
 ${AUTOCONF:-autoconf}
 ${LIBTOOLIZE:-libtoolize} --copy --force >/dev/null
